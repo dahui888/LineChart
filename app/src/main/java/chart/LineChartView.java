59739deeff6b5d47ee09a9379e3f0f7adeb2069d
daiwj <1298300385@qq.com>
2017-09-14 12:44:22 +0800

    优化

--- app/src/main/java/chart/LineChartView.java
+++ app/src/main/java/chart/LineChartView.java
@@ -47,6 +47,8 @@ public class LineChartView extends View {
      */
     private FloatValueView floatValueView;
 
+    private int COUNT, WIDTH, HEIGHT;
+
     /**
      * value 的最小值，最大值
      */
@@ -70,11 +72,13 @@ public class LineChartView extends View {
      */
     public void workWith(List<String> keys, List<Double> values) {
         if (keys != null && keys.size() != 0 && values != null && values.size() != 0) {
-            final int count = keys.size();
-            keyViewList = new ArrayList<>(count);
-            valueViewList = new ArrayList<>(count);
 
-            for (int i = 0; i < count; i++) {
+            COUNT = Math.min(keys.size(), values.size()); // 数据点数量
+
+            keyViewList = new ArrayList<>(COUNT);
+            valueViewList = new ArrayList<>(COUNT);
+
+            for (int i = 0; i < COUNT; i++) {
                 if (i == 0) {
                     isAllValueSame = true;
                 } else {
@@ -86,7 +90,7 @@ public class LineChartView extends View {
                     }
                 }
             }
-            for (int i = 0; i < count; i++) {
+            for (int i = 0; i < COUNT; i++) {
                 Double value = values.get(i);
                 if (i == 0) {
                     minValue = value;
@@ -95,7 +99,7 @@ public class LineChartView extends View {
                 }
                 maxValue = Math.max(value.doubleValue(), maxValue);
             }
-            for (int i = 0; i < count; i++) {
+            for (int i = 0; i < COUNT; i++) {
                 String key = keys.get(i);
                 KeyView keyView = new KeyView(getContext(), key);
                 keyViewList.add(keyView);
@@ -112,66 +116,66 @@ public class LineChartView extends View {
     /**
      * 开始绘制折线图
      */
-    private void work() {
-        postDelayed(new Runnable() {
+    public void work() {
+        post(new Runnable() {
             @Override
             public void run() {
                 isReadyForWork = true;
 
-                final int width = getMeasuredWidth();  // View的宽度
-                final int height = getMeasuredHeight(); // View的高度
+                WIDTH = getWidth();  // View的宽度
+                HEIGHT = getHeight(); // View的高度
 
-                hLine = new HorizontalLine(getContext(), width, height);
+                hLine = new HorizontalLine(getContext(), WIDTH, HEIGHT);
 
-                final int keyViewCount = keyViewList.size(); // key数量
+                final int averageWidth = WIDTH / COUNT; // 每个key可绘制的宽度
 
-                int averageWidth = width / keyViewCount; // 每个key可绘制的宽度
-                for (int i = 0; i < keyViewCount; i++) {
+                // 循环遍历，设置每个key的绘制起点
+                for (int i = 0; i < COUNT; i++) {
                     KeyView keyView = keyViewList.get(i);
-                    keyView.setTextCenter(averageWidth / 2 + i * averageWidth, getMeasuredHeight() - DensityUtil.dp2px(getContext(), 4)); // 循环遍历，设置每个key的绘制起点
+                    keyView.setTextCenter(averageWidth / 2 + i * averageWidth, HEIGHT - DensityUtil.dp2px(getContext(), 4));
                 }
 
-                /**
+                /*
                  * View当中，y坐标 是从上往下递增，而Value从下往上递增的
                  * value 越小，y坐标越大，切记，切记!
-                 **/
-                final int minY = getMeasuredHeight() * 2 / 8; // 最大值所在的y坐标，即最小的y坐标
-                final int maxY = getMeasuredHeight() * 6 / 8; // 最小值所在的y坐标，即最大的y坐标
+                 */
+                final int minY = HEIGHT * 2 / 8; // 最大值所在的y坐标，即最小的y坐标
+                final int maxY = HEIGHT * 6 / 8; // 最小值所在的y坐标，即最大的y坐标
                 final int validHeight = maxY - minY; // 可供绘制的有效区域
                 final double offsetValue = maxValue - minValue; // value最大值和最小值之差
-                for (int i = 0; i < keyViewCount; i++) {
+                for (int i = 0; i < COUNT; i++) {
                     ValueView valueView = valueViewList.get(i);
                     int centerX = averageWidth / 2 + i * averageWidth;
                     // 这里参照最小值所在的y坐标
                     int centerY;
                     if (isAllValueSame) {
-                        centerY = getMeasuredHeight() * 4 / 8;
+                        centerY = HEIGHT * 4 / 8;
                     } else {
                         centerY = (int) (minY + (maxValue - valueView.getValue()) / offsetValue * validHeight);
                     }
                     valueView.setCenter(centerX, centerY); // 循环遍历，设置每个value的绘制起点
                 }
 
-                final int lastKey = keyViewCount - 1;
+                final int last = COUNT - 1;
 
-                ValueView valueView = valueViewList.get(lastKey);
+                ValueView valueView = valueViewList.get(last);
                 verticalDashesLine = new VerticalDashesLine(getContext());
                 verticalDashesLine.setPoints(valueView.centerX,
                         valueView.centerY,
                         valueView.centerX,
-                        getMeasuredHeight() - DensityUtil.dp2px(getContext(), 20));
+                        HEIGHT - DensityUtil.dp2px(getContext(), 20));
 
-                floatValueView = new FloatValueView(getContext(), getWidth(), getHeight());
+                floatValueView = new FloatValueView(getContext(), WIDTH, HEIGHT);
                 floatValueView.attachValueView(valueView);
 
                 pathView = new PathView(getContext()); // 创建折线
                 pathView.lineValueViews(valueViewList); // 折线连接每个value
 
-                mSelectedIndex = lastKey;
+                mSelectedIndex = last;
 
                 startAnimationPath();
             }
-        }, 500);
+        });
     }
 
     @Override
@@ -231,8 +235,7 @@ public class LineChartView extends View {
             int y = (int) event.getY();
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    final int count = keyViewList.size();
-                    for (int i = 0; i < count; i++) {
+                    for (int i = 0; i < COUNT; i++) {
                         ValueView valueView = valueViewList.get(i);
                         if (valueView.isTouched(x, y) && mSelectedIndex != i) {
                             mSelectedIndex = i;
@@ -241,10 +244,10 @@ public class LineChartView extends View {
                             verticalDashesLine.setPoints(valueView.centerX,
                                     valueView.centerY,
                                     valueView.centerX,
-                                    getMeasuredHeight() - DensityUtil.dp2px(getContext(), 20));
+                                    HEIGHT - DensityUtil.dp2px(getContext(), 20));
 
                             floatValueView = new FloatValueView(getContext(), getWidth(), getHeight());
-                            if (mSelectedIndex == count - 1) {
+                            if (mSelectedIndex == COUNT - 1) {
                                 floatValueView.attachValueView(valueView);
                             } else {
                                 floatValueView.attachValueView(valueView);
